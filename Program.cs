11cb5eef7383a6f279248323d567dca23725747d
Thabo Chambule <110616731+ThaboChambule@users.noreply.github.com>
2024-10-30 09:59:42 +0200

    Add dimentions and grid

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Snake_Game;
+Coord gridDimensions = new Coord(50, 20);
+Coord snakePos = new Coord(10, 0);
+
+for (int y = 0; y < gridDimensions.Y; y++)
+{
+	for (int x = 0; x < gridDimensions.X; x++)
+	{
+		if(x==0 || y==0||x==gridDimensions.X - 1 || y==gridDimensions.Y -1)
+            Console.Write("#");
+		else
+		{
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+
+}

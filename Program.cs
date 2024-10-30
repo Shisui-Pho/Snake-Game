fcd7b24912842ede7a2951daf62d536aa5795595
Thabo Chambule <110616731+ThaboChambule@users.noreply.github.com>
2024-10-30 10:14:35 +0200

    Add apple which will be a target and make its position random

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Snake_Game;
 Coord gridDimensions = new Coord(50, 20);
 Coord snakePos = new Coord(10, 1);
+Random rand = new Random();
+Coord applePos = new Coord(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
 
 for (int y = 0; y < gridDimensions.Y; y++)
 {
@@ -10,7 +12,9 @@ for (int y = 0; y < gridDimensions.Y; y++)
 
         if(snakePos.Equals(currentCoord))
             Console.Write('■');
-		else if(x==0 || y==0||x==gridDimensions.X - 1 || y==gridDimensions.Y -1)
+        else if(applePos.Equals(currentCoord))
+            Console.Write("a");
+        else if(x==0 || y==0||x==gridDimensions.X - 1 || y==gridDimensions.Y -1)
             Console.Write("#");
 		else
 		{
